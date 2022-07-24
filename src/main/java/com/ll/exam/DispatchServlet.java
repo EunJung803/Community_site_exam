6c832afc0c66ee2d7ee1725c8ed5bd2593c8a192
EunJung803 <aimee3322@gmail.com>
2022-07-24 23:33:00 +0900

    라우팅(교통정리) 기능 구현

--- src/main/java/com/ll/exam/DispatchServlet.java
+++ src/main/java/com/ll/exam/DispatchServlet.java
@@ -1,5 +1,7 @@
 package com.ll.exam;
 
+import com.ll.exam.article.ArticleController;
+import com.ll.exam.member.MemberController;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,6 +13,24 @@ import java.io.IOException;
 public class DispatchServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.getWriter().append("Work?");
+        Rq rq = new Rq(req, resp);
+
+        MemberController memberController = new MemberController();
+        ArticleController articleController = new ArticleController();
+
+        String url = req.getRequestURI();
+        // getRequestURI는
+        // 예를 들어, 전체 url "http://localhost:8081/usr/article/list/free?page=1" 중 에서
+        // 중간에 있는 "/usr/article/list/free" 딱 이 부분만 가져온다.
+
+        switch (url) {
+            case "/usr/article/list/free":
+                articleController.showList(rq);
+                break;
+
+            case "/usr/member/login":
+                memberController.showLogin(rq);
+                break;
+        }
     }
 }

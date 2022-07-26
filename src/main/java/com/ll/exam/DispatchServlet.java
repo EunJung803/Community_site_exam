44218d5fd45510d96110bedd13d4feb9ec6f8992
EunJung803 <aimee3322@gmail.com>
2022-07-26 14:38:00 +0900

    POST 방식으로 입력된 게시물 작성 데이터 확인

--- src/main/java/com/ll/exam/DispatchServlet.java
+++ src/main/java/com/ll/exam/DispatchServlet.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 @WebServlet("/usr/*")
 public class DispatchServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
         Rq rq = new Rq(req, resp);
 
         MemberController memberController = new MemberController();
@@ -23,18 +23,35 @@ public class DispatchServlet extends HttpServlet {
         // 예를 들어, 전체 url "http://localhost:8081/usr/article/list/free?page=1" 중 에서
         // 중간에 있는 "/usr/article/list/free" 딱 이 부분만 가져온다.
 
-        switch (url) {
-            case "/usr/article/list/free":
-                articleController.showList(rq);
+        switch (rq.getMethod()) {
+            case "GET":
+                switch (rq.getPath()) {
+                    case "/usr/article/list/free":
+                        articleController.showList(rq);
+                        break;
+
+                    case "/usr/member/login":
+                        memberController.showLogin(rq);
+                        break;
+
+                    case "/usr/article/write/free":
+                        articleController.showWrite(rq);
+                        break;
+                }
                 break;
 
-            case "/usr/member/login":
-                memberController.showLogin(rq);
-                break;
-
-            case "/usr/article/write/free":
-                articleController.showWrite(rq);
+            case "POST":
+                switch (rq.getPath()) {
+                    case "/usr/article/write/free":
+                        articleController.doWrite(rq);
+                        break;
+                }
                 break;
         }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        doGet(req, resp);
     }
 }

57c7acf0339c0367bbcfb8fac45613874c32ce18
EunJung803 <aimee3322@gmail.com>
2022-07-27 11:28:53 +0900

    게시물 테스트 페이지 자동생성 + 수정 삭제 링크 추가

--- src/main/java/com/ll/exam/article/ArticleRepository.java
+++ src/main/java/com/ll/exam/article/ArticleRepository.java
@@ -4,6 +4,7 @@ import com.ll.exam.article.dto.ArticleDto;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class ArticleRepository {
     private static List<ArticleDto> datum;
@@ -12,9 +13,21 @@ public class ArticleRepository {
     static {
         datum = new ArrayList<>();
         lastID = 0;
+
+        makeTestData();     // 프로그램 실행 시 데이터가 채워진다.
     }
 
-    public long write(String title, String body) {
+    // 매번 테스트를 하기 번거로우니 생성
+    private static void makeTestData() {
+        // for문과 같음, 이 함수가 10번 반복된다.
+        IntStream.rangeClosed(1, 10).forEach(id -> {
+            String title = "제목 %d".formatted(id);
+            String body = "내용 %d".formatted(id);
+            write(title, body);
+        });
+    }
+
+    public static long write(String title, String body) {
         long id = ++lastID;
         ArticleDto newArticleDto = new ArticleDto(id, title, body);
         datum.add(newArticleDto);
@@ -22,11 +35,11 @@ public class ArticleRepository {
         return id;
     }
 
-    public List<ArticleDto> findAll() {
+    public static List<ArticleDto> findAll() {
         return datum;
     }
 
-    public ArticleDto findById(long id) {
+    public static ArticleDto findById(long id) {
         for ( ArticleDto articleDto : datum ) {
             if ( articleDto.getId() == id ) {
                 return articleDto;
--- src/main/webapp/jsp/usr/article/list.jsp
+++ src/main/webapp/jsp/usr/article/list.jsp
@@ -11,7 +11,9 @@
 <ul>
     <% for ( ArticleDto article : articles ) { %>
     <li>
-        <a href="/usr/article/detail/free/<%=article.getId()%>"><%=article.getId()%>. <%=article.getTitle()%></a>
+        <a href="/usr/article/detail/free/<%=article.getId()%>"></a>
+        <a href="/usr/article/modify/free/<%=article.getId()%>">수정</a>
+        <a href="/usr/article/delete/free/<%=article.getId()%>">삭제</a>
     </li>
     <% } %>
 </ul>
